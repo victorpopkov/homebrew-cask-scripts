6bf6ff57ae7477d5aa5ff4bd1109e8463633bc13
Victor Popkov <victor@popkov.me>
2016-06-04 23:19:47 +0300

    cask-check-updates 0.4.8

--- cask-check-updates.rb
+++ cask-check-updates.rb
@@ -2,13 +2,13 @@ class CaskCheckUpdates < Formula
   desc "Script to find outdated casks and their latest versions"
   homepage "https://github.com/victorpopkov/cask-scripts"
   url "https://github.com/victorpopkov/cask-scripts.git"
-  version "0.4.7"
+  version "0.4.8"
 
   depends_on "xmlstarlet"
   depends_on "jq"
-  depends_on "hr" => :recommended
 
   def install
+    system "make", "install", "PREFIX=#{prefix}"
     bin.install "bin/cask-check-updates"
   end
 
