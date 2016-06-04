7f5a3e1b97731e7e4abe0ceddabd6414c2bf03eb
Victor Popkov <victor@popkov.me>
2016-06-04 16:56:08 +0300

    cask-appcast 0.3.6

--- cask-appcast.rb
+++ cask-appcast.rb
@@ -2,14 +2,14 @@ class CaskAppcast < Formula
   desc "Script to get the latest available version from appcast URL"
   homepage "https://github.com/victorpopkov/cask-scripts"
   url "https://github.com/victorpopkov/cask-scripts.git"
-  version "0.3.5"
+  version "0.3.6"
 
   depends_on "xmlstarlet"
   depends_on "jq"
   depends_on "hr" => :recommended
 
   def install
-    bin.install "cask-appcast"
+    bin.install "bin/cask-appcast"
   end
 
   test do
