e747bbd8af19cb271e16411b30da163c1387660d
Victor Popkov <victor@popkov.me>
2016-06-23 14:56:11 +0300

    Merge cask-appcast and cask-check-updates into cask-scripts

---
+++ Formula/cask-scripts.rb
@@ -0,0 +1,17 @@
+class CaskScripts < Formula
+  desc "Collection of small scripts to help maintain the Homebrew-Cask project"
+  homepage "https://github.com/victorpopkov/cask-scripts"
+  url "https://github.com/victorpopkov/cask-scripts.git"
+  version "0.1"
+
+  depends_on "xmlstarlet"
+  depends_on "jq"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "#{prefix}/test/bats/bin/bats", "#{prefix}/test/"
+  end
+end
