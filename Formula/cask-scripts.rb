7f7a672e5e16205632fff19e9eaa137aa5b63beb
Victor Popkov <victor@popkov.me>
2016-11-27 20:59:04 +0200

    cask-scripts 0.4.6

--- Formula/cask-scripts.rb
+++ Formula/cask-scripts.rb
@@ -2,7 +2,7 @@ class CaskScripts < Formula
   desc 'Collection of small scripts to help maintain the Homebrew-Cask project'
   homepage 'https://github.com/victorpopkov/cask-scripts'
   url 'https://github.com/victorpopkov/cask-scripts.git'
-  version '0.4.5'
+  version '0.4.6'
 
   depends_on 'xmlstarlet'
   depends_on 'jq'
